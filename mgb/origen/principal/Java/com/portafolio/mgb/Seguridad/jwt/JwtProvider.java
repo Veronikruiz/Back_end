e5d2a4a2ff500aa8ea646decf2307411636b1055
Veronikruiz <94694680+Veronikruiz@users.noreply.github.com>
2022-09-03 20:06:50 -0300

    Update and rename mgb/src/main/java/com/portfolio/mgb/Security/jwt/JwtProvider.java to mgb/origen/principal/Java/com/portafolio/mgb/Seguridad/jwt/JwtProvider.java

--- mgb/origen/principal/Java/com/portafolio/mgb/Seguridad/jwt/JwtProvider.java
+++ mgb/origen/principal/Java/com/portafolio/mgb/Seguridad/jwt/JwtProvider.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -28,7 +28,7 @@ public class JwtProvider {
     @Value("${jwt.expiration}")
     private int expiration;
 
-    public String generateToke(Authentication authentication) {
+    public String generateToken(Authentication authentication) {
         UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
         return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
                 .setIssuedAt(new Date())
